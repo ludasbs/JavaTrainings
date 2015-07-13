dd253ce38a973dab17c3b6d021ed4a33061522e6
Luidmila Simak <simak-2016@mail.ru>
2015-07-13 11:43:00 +0300

    Boll

--- simak/Boll/Bol.java
+++ simak/Boll/Bol.java
@@ -5,6 +5,8 @@ package Boll;
  */
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Bol{
 
@@ -14,10 +16,42 @@ public class Bol{
 
 
 
+        final Graphics g = myWindow.getGraphics();
+
+        // TODO разобраться почему здесь использованы массивы из одного элемента
+        // TODO улучшить, т.е. хранить эти параметны в некотором объекте
+        final int[] x = {50};
+        final int[] y = {150};
+
+        final int size = 50;
+        // HINT: нужно добавить шаг по OX
+        final int[] step = {2};
+
+        Timer timer = new Timer(20, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Clear old image
+
+                g.clearRect(0,0, 300,460);
+                g.setColor(new Color(4, 81, 40));
+                g.fillOval(x[0], y[0],size,size);
+                y[0] += step[0];
+
+                // HINT нужно проверять и по оси OX
+                if(y[0] > 300-size) {
+                    step[0] =-step[0];
+                } else if(y[0]<size/2) {
+                    step[0] =-step[0];
+                }
+            }
+        });
+
+        timer.start();
+    }
 
 
     }
-}
+
 
 
 
